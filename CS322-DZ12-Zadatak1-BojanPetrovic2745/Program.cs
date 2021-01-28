2c53d50148faf0108a8f2be57a374273bb09d749
bojan <bojan.petrovic.2745@outlook.com>
2021-01-29 00:13:27 +0100

    Add project files.

---
+++ CS322-DZ12-Zadatak1-BojanPetrovic2745/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace CS322_DZ12_Zadatak1_BojanPetrovic2745
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // potrebno je koristi xampp
+
+            MySqlConnection connection = new MySqlConnection("Server=127.0.0.1;Database=cs322;Uid=root;Pwd=;");
+
+            connection.Open();
+
+            String sql = "INSERT INTO korisnik (username,password)VALUES('nesto', 'nesto')";
+            
+            MySqlCommand comm = new MySqlCommand(sql, connection);
+            try
+            {
+                comm.ExecuteNonQuery();
+            }
+            catch(Exception e){
+                Console.WriteLine("Error");
+            }
+            finally { connection.Close(); }
+        }
+    }
+}
